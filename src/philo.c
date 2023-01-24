a09e24be729e06fa9fbc3b62f44611edcd582418
MaxIhme <maxrehberg@posteo.de>
2023-01-24 12:00:00 +0100

    little diff

--- src/philo.c
+++ src/philo.c
@@ -6,7 +6,7 @@
 /*   By: mrehberg <maxrehberg@posteo.de>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 18:12:41 by mrehberg          #+#    #+#             */
-/*   Updated: 2023/01/24 10:57:49 by mrehberg         ###   ########.fr       */
+/*   Updated: 2023/01/24 11:59:37 by mrehberg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,24 +37,3 @@ int	main(int argc, char **argv)
 	clean_up(CLEAN_UP_EVERYTHING, &info);
 	return (0);
 }
-
-//timingtest anruland
-
-//verschiedenen input testen minus 0 buchstaben etc.
-	// auf leaks überprüfen
-
-	//init aufräumen und die errors vervollständigen
-
-// Philo test
-// - Do not test with more than 200 philosophers
-// - Do not test with time_to_ die or time_to_eat or time_to_sleep under 60 ms
-// - Test with 1 800 200 200, the philosopher should not eat and should die!
-// - Test with 5 800 200 200, no one should die!
-// - Test with 5 800 200 200 7, no one should die and the simulation 
-	// should stop when all the philosopher has eaten at least 7 times each.
-// - Test with 4 410 200 200, no one should die!
-// - Test with 4 310 200 100, a philosopher should die!
-// - Test with 2 philosophers and check the different times (a death delayed 
-	// by more than 10 ms is unacceptable).
-// - Test with your values to check all the rules. Check if a philosopher 
-	// dies at the right time if they don't steal forks, etc.
